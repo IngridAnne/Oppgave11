dde445e9cac53af684f998f82f44c3a16ec453e4
IngridAnne <ingridanne3009@gmail.com>
2026-09-08 17:34:06 +0200

    Fjernet testkoden

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  8 17:04:18 2026
 
-@author: ingridanneomtveitlundem
-"""
-
-print("tester enda en gang")
+print("Oppgave 11")
